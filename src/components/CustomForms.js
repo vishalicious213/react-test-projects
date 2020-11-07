4b4da4349e975f9334b3aef344b76e7d6b9210ea
Vish Singh <vishalicious213@gmail.com>
2020-11-07 14:30:56 -0800

    styled custom-forms

--- src/components/CustomForms.js
+++ src/components/CustomForms.js
@@ -23,56 +23,60 @@ function CustomForms() {
     return (
         <div id='custom-forms'>
             <form>
-                <div>
-                    <label>
-                        Name:
-                        <input type="text" onChange={event => handleChange(event)} />
+                <div className='form-field wide'>
+                    <label id='custom-forms-name-box'>
+                        <span className='field-name'>Name:</span>
+                        <input id='custom-forms-name' type="text" onChange={event => handleChange(event)} />
                     </label>
-                    <button onSubmit={() => handleSubmit()}>Submit!</button>
+                    {/* <button onSubmit={() => handleSubmit()}>Submit!</button> */}
                 </div>
 
-                <div>
-                    <span> Age Range: </span>
-                    <label>
+                <div className='form-field'>
+                    <span className='field-name'>Age Range:</span>
+                    <label className='age-range'>
                         <input type="radio" name="age" value='13-18' onChange={event => handleChange(event)} />
                         13-18
                     </label>
-                    <label>
+                    <label className='age-range'>
                         <input type="radio" name="age" value='19-24' onChange={event => handleChange(event)} />
                         19-24
                     </label>
-                    <label>
+                    <label className='age-range'>
                         <input type="radio" name="age" value='25-39' onChange={event => handleChange(event)} />
                         25-39
                     </label>
-                    <label>
+                    <label className='age-range'>
                         <input type="radio" name="age" value='40+' onChange={event => handleChange(event)} />
                         40+
                     </label>
                 </div>
 
-                <div>
-                    <span>Home State:</span>
-                    <select id="states" name="states">
-                        <option value="Alabama">Alabama</option>
-                        <option value="Alaska">Alaska</option>
-                        <option value="Arizona">Arizona</option>
-                        <option value="Arkansas">Arkansas</option>
-                        <option value="Colorado">Colorado</option>
-                    </select>
-                </div>
+                <div className='wide'>
+                    <label className='form-field'>
+                        <span className='field-name'>Home State:</span>
+                        <select id="states" name="states">
+                            <option value="Alabama">Alabama</option>
+                            <option value="Alaska">Alaska</option>
+                            <option value="Arizona">Arizona</option>
+                            <option value="Arkansas">Arkansas</option>
+                            <option value="Colorado">Colorado</option>
+                        </select>
+                    </label>
 
-                <div>
-                    <label>
-                        <input
-                            name="isGoing"
-                            type="checkbox"
-                            checked={false}
-                            onChange={handleInputChange}
-                        />
-                        RSVP
+                    <label className='form-field'>
+                        {/* <label> */}
+                            <input
+                                name="isGoing"
+                                type="checkbox"
+                                checked={false}
+                                onChange={handleInputChange}
+                            />
+                            <span>RSVP</span>
+                        {/* </label> */}
                     </label>
                 </div>
+
+                <button id='custom-forms-submit' onSubmit={() => handleSubmit()}>Submit!</button>
             </form>
         </div>
     )
