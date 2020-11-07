25300b52e569bcb52935fcbc30dbeb99d0724bf9
Vish Singh <vishalicious213@gmail.com>
2020-11-07 01:20:40 -0800

    updated home

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,9 +4,10 @@ import logo from '../logo.svg';
 function Home() {
   return (
     <div>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>      
+        <h1 id='title'>REACT TEST PROJECTS</h1>
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+        </header>      
     </div>
   );
 }
