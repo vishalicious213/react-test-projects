dcd26017400d33851823eb8905ff076c59a4297e
vishalicious213 <vishalicious213@gmail.com>
2021-02-16 13:54:45 -0800

    styling FileUpload

--- src/components/upload-components/FileUpload.js
+++ src/components/upload-components/FileUpload.js
@@ -3,13 +3,13 @@ import React from 'react'
 const FileUpload = () => {
     return (
         <>
-            <form>
-                <div className='custom-file'>
+            <form className='upload-form'>
+                <div className='file-upload'>
                     <input type='file' className='custom-file-input' id='customFile' />
                     <label className='custom-file-label' htmlFor='customFile'>Choose file</label>
                 </div>
 
-                <input type='submit' value='Upload' className='btn' />
+                <input type='submit' value='Upload' className='upload-button' />
             </form>
         </>
     )
