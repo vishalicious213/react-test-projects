a0b2bf1ecc4883015e72d89c66b03c6f45c96e65
vishalicious213 <vishalicious213@gmail.com>
2021-02-16 13:30:24 -0800

    started upload-components/FileUpload

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import '../App.css'
 
-const Upload = () => <div className='Upload'>Upload</div>
+// const Upload = () => <div className='Upload'>Upload</div>
 
-// const Upload = () => {
-//     return (
-//         <div className='Upload'>Upload</div>
-//     )
-// }
+const Upload = () => {
+    return (
+        <div className='Upload'>
+            <h4>Upload</h4>
+        </div>
+    )
+}
 
 export default Upload
