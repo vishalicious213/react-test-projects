dac0ece39fe2828bf256eb5396d44d24b8209e73
vishalicious213 <vishalicious213@gmail.com>
2021-02-16 12:39:00 -0800

    server.js - adding upload endpoint

--- server.js
+++ server.js
@@ -11,6 +11,17 @@ app.post('/upload', (req, res) => {
     if(req.files === null) {
         return res.status(400).json({ msg: 'No file uploaded' })
     }
+
+    const file = req.files.file // define the file in React
+
+    file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
+        if(err) {
+            console.error(err)
+            return res.status(500).send(err)
+        }
+
+        res.json({ fileName: file.name, filePath: `/uploads/${file.name}`})
+    })
 })
 
 app.listen(5000, () => console.log('File Upload server started'))
