4c2f8fc3d7e13ef1067ba6ea1835b207385ccd07
vishalicious213 <vishalicious213@gmail.com>
2021-02-16 15:53:41 -0800

    FileUpload - added onChange

--- src/components/upload-components/FileUpload.js
+++ src/components/upload-components/FileUpload.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const FileUpload = () => {
+    const [file, setFile] = useState('')
+    const [filename, setFilename] = useState('Choose file')
+
+    const onChange = e => {
+        setFile(e.target.files[0]);
+        setFilename(e.target.files[0].name)
+    }
+
     return (
         <>
             <form className='upload-form'>
                 <div className='file-upload'>
                     <label className='upload-input-label' htmlFor='uploadFile'>Choose file</label>
-                    <input type='file' className='upload-input' id='uploadFile' />
+                    <input type='file' className='upload-input' id='uploadFile' onChange={onChange} />
                 </div>
 
                 <input type='submit' value='Upload' className='upload-button' />
