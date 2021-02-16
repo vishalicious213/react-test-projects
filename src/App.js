7cd04ce8ac87e0ad6b255c046dab2c7cb1aafadb
vishalicious213 <vishalicious213@gmail.com>
2021-02-16 13:02:04 -0800

    started Upload component. added it to router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Menu from './components/Menu'
 import Home from './components/Home'
 import ControlledInputs from './components/ControlledInputs'
 import CustomForms from './components/CustomForms'
+import Upload from './components/Upload'
 import './App.css';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Route exact path ='/' component={Home}/>
       <Route path='/controlled' component={ControlledInputs}/>
       <Route path='/customforms' component={CustomForms}/>
+      <Route path='/upload' component={Upload}/>
     </div>
   );
 }
---
+++ src/components/Upload.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../App.css'
+
+const Upload = () => <div className='Upload'>Upload</div>
+
+// const Upload = () => {
+//     return (
+//         <div className='Upload'>Upload</div>
+//     )
+// }
+
+export default Upload
