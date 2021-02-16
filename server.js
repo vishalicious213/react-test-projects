3dcfd2a4b0bed6e93d060366d1a25448bd93534f
vishalicious213 <vishalicious213@gmail.com>
2021-02-16 09:57:31 -0800

    server.js - adding upload endpoint

--- server.js
+++ server.js
@@ -6,4 +6,11 @@ const fileUpload = require('express-fileupload')
 const app = express()
 app.use(fileUpload())
 
+// Upload endpoint
+app.post('/upload', (req, res) => {
+    if(req.files === null) {
+        return res.status(400).json({ msg: 'No file uploaded' })
+    }
+})
+
 app.listen(5000, () => console.log('File Upload server started'))
