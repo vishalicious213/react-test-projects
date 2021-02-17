bbe25d8df008bbebf2cd8d1bc4143dbbece092fc
vishalicious213 <vishalicious213@gmail.com>
2021-02-16 21:08:43 -0800

    FileUpload - gave explicit address to axios call, server.js - added CORS middleware and changed path to save file, file directory - added uploads folder to public

--- server.js
+++ server.js
@@ -6,15 +6,21 @@ const fileUpload = require('express-fileupload')
 const app = express()
 app.use(fileUpload())
 
+// CORS
+let allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    next();
+  };
+
 // Upload endpoint
-app.post('/upload', (req, res) => {
+app.post('/upload', allowCrossDomain, (req, res) => {
     if(req.files === null) {
         return res.status(400).json({ msg: 'No file uploaded' })
     }
 
     const file = req.files.file // define the file in React
 
-    file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
+    file.mv(`${__dirname}/public/uploads/${file.name}`, err => {
         if(err) {
             console.error(err)
             return res.status(500).send(err)
--- src/components/upload-components/FileUpload.js
+++ src/components/upload-components/FileUpload.js
@@ -4,31 +4,43 @@ import axios from 'axios'
 const FileUpload = () => {
     const [file, setFile] = useState('')
     const [filename, setFilename] = useState('Choose file')
+    const [uploadedFile, setUploadedFile] = useState({})
 
     const onChange = e => {
+        // console.log('file: ', e.target.files[0])
         setFile(e.target.files[0]);
         setFilename(e.target.files[0].name)
+        // console.log(file)
     }
 
     const onSubmit = async e => {
         e.preventDefault()
+        console.log('state: ', file)
         const formData = new FormData()
         formData.append('file', file)
 
         try {
-            const res = await axios.post('/upload', formData, {
+            const res = await axios.post('http://localhost:5000/upload', formData, {
                 headers: {
                     'Content-Type': 'multipart/form-data'
                 }
             })
-        } catch(err) {
 
+            const { fileName, filePath } = res.data
+
+            setUploadedFile({ fileName, filePath })
+        } catch(err) {
+            if(err.response.status === 500) {
+                console.log('There was a problem with the server')
+            } else {
+                console.log(err.response.data.msg)
+            }
         }
     }
 
     return (
         <>
-            <form className='upload-form'>
+            <form className='upload-form' onSubmit={onSubmit}>
                 <div className='file-upload'>
                     <label className='upload-input-label' htmlFor='uploadFile'>Choose file</label>
                     <input type='file' className='upload-input' id='uploadFile' onChange={onChange} />
