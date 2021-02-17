baef4d3802caa6ee7ea3ca5408ce67347704e698
vishalicious213 <vishalicious213@gmail.com>
2021-02-16 16:00:54 -0800

    FileUpload - added onSubmit

--- src/components/upload-components/FileUpload.js
+++ src/components/upload-components/FileUpload.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import axios from 'axios'
 
 const FileUpload = () => {
     const [file, setFile] = useState('')
@@ -7,6 +8,22 @@ const FileUpload = () => {
     const onChange = e => {
         setFile(e.target.files[0]);
         setFilename(e.target.files[0].name)
+    }
+
+    const onSubmit = async e => {
+        e.preventDefault()
+        const formData = new FormData()
+        formData.append('file', file)
+
+        try {
+            const res = await axios.post('/upload', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            })
+        } catch(err) {
+
+        }
     }
 
     return (
