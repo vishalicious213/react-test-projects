0c0d3daffca22c958f910b8ec2d99ed9f98aa711
vishalicious213 <vishalicious213@gmail.com>
2021-02-20 21:21:20 -0800

    drafted PortalModal, imported into Portal

--- src/components/Portal.js
+++ src/components/Portal.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import PortalModal from './portal-components/PortalModal'
 
 export default function Portal() {
     return (
         <>
         <div className='portal'>
             <button>Open Modal</button>
+
+            <PortalModal>
+                MODAL
+            </PortalModal>
         </div>
 
         <div className='other'>Other Content</div>
