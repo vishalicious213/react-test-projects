caad88a5521c776d5ee74f0bc18f810e33fa33b7
vishalicious213 <vishalicious213@gmail.com>
2021-02-20 22:25:49 -0800

    Portal - imported ControlledInputs component and rendered it inside of PortalModal! We can render multiple component modals!

--- src/components/Portal.js
+++ src/components/Portal.js
@@ -1,16 +1,27 @@
 import React, { useState } from 'react'
 import PortalModal from './portal-components/PortalModal'
 import Upload from './Upload'
+import ControlledInputs from './ControlledInputs'
 
 export default function Portal() {
     const [isOpen, setIsOpen] = useState(false)
+    const [isOpenB, setIsOpenB] = useState(false)
+
     return (
         <>
         <div className='portal'>
-            <button onClick={() => setIsOpen(true)}>Open Modal</button>
+            <button onClick={() => setIsOpen(true)}>Open Upload Modal</button>
 
             <PortalModal open={isOpen} onClose={() => setIsOpen(false)}>
                 <Upload />
+            </PortalModal>
+        </div>
+
+        <div className='portal'>
+            <button onClick={() => setIsOpenB(true)}>Open Inputs Modal</button>
+
+            <PortalModal open={isOpenB} onClose={() => setIsOpenB(false)}>
+                <ControlledInputs />
             </PortalModal>
         </div>
 
