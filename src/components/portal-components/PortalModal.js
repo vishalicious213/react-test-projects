b0a5c02d0470112b56e96be957f3b3577ffcb6b0
vishalicious213 <vishalicious213@gmail.com>
2021-02-20 21:50:14 -0800

    PortalModal - imported ReactDom and rendered component with createPortal (added div with id of 'portal' as anchor element

--- src/components/portal-components/PortalModal.js
+++ src/components/portal-components/PortalModal.js
@@ -1,15 +1,17 @@
 import React from 'react'
+import ReactDom from 'react-dom'
 
 export default function PortalModal({ open, children, onClose }) {
     if (!open) return null
 
-    return (
+    return ReactDom.createPortal(
         <>
         <div className='portalOverlay'></div>
         <div className='portalModal'>
             <button onClick={onClose}>Close Modal</button>
             {children}
         </div>
-        </>
+        </>,
+        document.getElementById('portal')
     )
 }
