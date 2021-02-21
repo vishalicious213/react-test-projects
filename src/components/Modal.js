ee46fadd043204a593d67156bdd1ca80fe949534
vishalicious213 <vishalicious213@gmail.com>
2021-02-20 20:08:04 -0800

    added Modal

---
+++ src/components/Modal.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState, useRef } from 'react'
+import '../App.css'
+
+function Modal() {
+    const outside = useRef()
+    const [isOpen, setIsOpen] = useState(false)
+
+    const handleClick = e => {
+        if (outside.current.contains(e.target)) {
+            return
+        }
+        setIsOpen(false)
+    }
+
+    useEffect(() => {
+        const getClick = document.addEventListener('click', handleClick)
+
+        return () => {
+            getClick()
+        }
+    }, [])
+
+    return (
+        <div className='Modal' ref={outside}>
+            <button onClick={() => setIsOpen(!isOpen)}>Toggle Modal</button>
+            {isOpen ? (
+                <div className='modal'>
+                    <p>Modal is open</p>
+                </div>
+            ) : null}
+        </div>
+    )
+}
+
+export default Modal
