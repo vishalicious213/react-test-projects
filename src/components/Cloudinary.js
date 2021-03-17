b505329e394898126fea82fdb4b484af181973fc
vishalicious213 <vishalicious213@gmail.com>
2021-03-16 21:05:59 -0700

    Cloudinary takes files and console.logs the first one from the array

--- src/components/Cloudinary.js
+++ src/components/Cloudinary.js
@@ -2,9 +2,18 @@ import React from 'react'
 import '../App.css'
 
 const Cloudinary = () => {
+    const uploadImage = (files) => {
+        console.log(files[0])
+    }
+
     return (
         <div id='cloudinary'>
-
+            <input 
+                type='file' 
+                onChange ={(event) => {
+                    uploadImage(event.target.files)
+                }}
+            />
         </div>
     )
 }
